f49b521552ee097bd900a7e577a75cce25c63f65
weaton5061 <weaton5061@gmail.com>
2020-07-12 12:02:14 -0500

    Original Commit, new repository

---
+++ Nigeria Chloropleth Map Creation Code.R
@@ -0,0 +1,9 @@
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Nigeria new chlorpleth map code
+# Author: Will Eaton
+# Date created: 7-12-20
+# Date modified: 7-12-20
+# Local drive location: /Volumes/Samsung T5/LACIE MacOS Extended/Tulane Research Projects/Malaria Consortium/Methodology/R Code/MC R Code/Malaria-Consortium
+# git url: https://github.com/weaton5061/Malaria-Consortium.git
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
